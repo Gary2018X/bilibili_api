f7d8e88c9b3b166df51c82c988ae54661a445f26
Gary <56428019+Gary2018X@users.noreply.github.com>
2020-10-27 08:36:14 +0800

    3.0.0

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-from bilibili_api.user import get_up_info
+from bilibili_api import video
+import json
 
-season_id = 64729846
-print(get_up_info(uid=season_id))
+v = video.get_video_info(bvid="BV1uv411q7Mv")
+
+print(json.dumps(v, indent=4, ensure_ascii=False))
